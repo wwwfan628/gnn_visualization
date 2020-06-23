ab7d4599695374d23cdb24fb111dc5452690641a
wwwfan628 <wwwfan628@163.com>
2020-06-23 09:22:48 +0200

    add ppi

--- src/run/embedding_identifiability.py
+++ src/run/embedding_identifiability.py
@@ -15,25 +15,43 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("RUNNING ON: {}".format(device))
 
 
-def compute_nodes_attribution(net, graph, features, layer, args):
+def compute_nodes_attribution_citation(net, graph, features, layer, args):
     max_contributed_input_nodes = np.zeros(features.shape[0])
-    if args.dataset in 'cora, citeseer, pubmed':
-        inputs = features.clone().detach().requires_grad_(True).to(device)
-        net.eval()
-        embedding = net(graph,inputs)[-layer]
-        inputs_gradient = np.zeros([embedding.shape[1], inputs.shape[0], inputs.shape[1]])
-        for embedding_node_id in range(embedding.shape[0]):
-            # compute gradient for embedding[embedding_node_id,:] wrt inputs[input_node_id,:]
-            for embedding_col_id in range(embedding.shape[1]):
-                gradients = torch.zeros(embedding.shape).float()
-                gradients[embedding_node_id,embedding_col_id] = 1.0
-                embedding.backward(gradient=gradients,retain_graph=True)
-                inputs_gradient[embedding_col_id,:,:] = inputs.grad
-                inputs.grad.data.zero_() # set gradient to 0
-            inputs_contribution_denormalized = np.linalg.norm(inputs_gradient,axis=(0,2),ord='fro')
-            max_contributed_input_nodes[embedding_node_id] = np.argmax(inputs_contribution_denormalized)
-            print('Node id {} | Node Contributed At Most {}'.format(embedding_node_id, max_contributed_input_nodes[embedding_node_id]))
+    inputs = features.clone().detach().requires_grad_(True).to(device)
+    net.eval()
+    embedding = net(graph,inputs)[-layer]
+    inputs_gradient = np.zeros([embedding.shape[1], inputs.shape[0], inputs.shape[1]])
+    for embedding_node_id in range(embedding.shape[0]):
+        # compute gradient for embedding[embedding_node_id,:] wrt inputs[input_node_id,:]
+        for embedding_col_id in range(embedding.shape[1]):
+            gradients = torch.zeros(embedding.shape).float()
+            gradients[embedding_node_id,embedding_col_id] = 1.0
+            embedding.backward(gradient=gradients,retain_graph=True)
+            inputs_gradient[embedding_col_id,:,:] = inputs.grad
+            inputs.grad.data.zero_() # set gradient to 0
+        inputs_contribution_denormalized = np.linalg.norm(inputs_gradient,axis=(0,2),ord='fro')
+        max_contributed_input_nodes[embedding_node_id] = np.argmax(inputs_contribution_denormalized)
+        print('Node id {} | Node Contributed At Most {}'.format(embedding_node_id, max_contributed_input_nodes[embedding_node_id]))
     return max_contributed_input_nodes
+
+# def compute_nodes_attribution_ppi(net, train_dataset, layer, args):
+#     max_contributed_input_nodes = np.array([])
+#     inputs = features.clone().detach().requires_grad_(True).to(device)
+#     net.eval()
+#     embedding = net(graph,inputs)[-layer]
+#     inputs_gradient = np.zeros([embedding.shape[1], inputs.shape[0], inputs.shape[1]])
+#     for embedding_node_id in range(embedding.shape[0]):
+#         # compute gradient for embedding[embedding_node_id,:] wrt inputs[input_node_id,:]
+#         for embedding_col_id in range(embedding.shape[1]):
+#             gradients = torch.zeros(embedding.shape).float()
+#             gradients[embedding_node_id,embedding_col_id] = 1.0
+#             embedding.backward(gradient=gradients,retain_graph=True)
+#             inputs_gradient[embedding_col_id,:,:] = inputs.grad
+#             inputs.grad.data.zero_() # set gradient to 0
+#         inputs_contribution_denormalized = np.linalg.norm(inputs_gradient,axis=(0,2),ord='fro')
+#         max_contributed_input_nodes[embedding_node_id] = np.argmax(inputs_contribution_denormalized)
+#         print('Node id {} | Node Contributed At Most {}'.format(embedding_node_id, max_contributed_input_nodes[embedding_node_id]))
+#     return max_contributed_input_nodes
 
 
 def main(args):
@@ -72,16 +90,16 @@ def main(args):
 
     # store embedding id rates for different models, row:model  column:embedding layer
     models = ['gcn_2layers', 'gcn_3layers', 'gcn_4layers', 'gcn_5layers', 'gcn_6layers']
-    embedding_id_rates_df = pd.DataFrame(np.zeros([6,5]), index=np.arange(1,7), columns=models)
-    node_repeatability_rates_df = pd.DataFrame(np.zeros([6, 5]), index=np.arange(1, 7), columns=models)
-    max_contributed_neighbour_recovered_rates_df = pd.DataFrame(np.zeros([6, 5]), index=np.arange(1, 7), columns=models)
+    embedding_id_rates_df = pd.DataFrame(np.zeros([6, len(models)]), index=np.arange(1,7), columns=models)
+    node_repeatability_rates_df = pd.DataFrame(np.zeros([6, len(models)]), index=np.arange(1, 7), columns=models)
+    max_contributed_neighbour_recovered_rates_df = pd.DataFrame(np.zeros([6, len(models)]), index=np.arange(1, 7), columns=models)
     for gcn_model_layer in np.arange(2, 7):
         # store embedding id rates for one model, row:embedding layer  column:experiment iteration time
-        embedding_id_rates = np.zeros([6,5])
+        embedding_id_rates = np.zeros([6,args.repeat_times])
         # node repeatablity rates for one model
-        node_repeatablity_rates = np.zeros([6,5])
+        node_repeatability_rates = np.zeros([6,args.repeat_times])
         # if not identifiable, the rates of identifying the neighbourhood node which contributes at most
-        max_contributed_neighbour_recovered_rates = np.zeros([6,5])
+        max_contributed_neighbour_recovered_rates = np.zeros([6,args.repeat_times])
         for repeat_time in np.arange(args.repeat_times):
             print("********** GCN MODEL: GCN_{}layer **********".format(gcn_model_layer))
             print("********** EXPERIMENT ITERATION: {} **********".format(repeat_time+1))
@@ -100,7 +118,7 @@ def main(args):
             print("********** GCN MODEL: GCN_{}layer **********".format(gcn_model_layer))
             print("********** EXPERIMENT ITERATION: {} **********".format(repeat_time+1))
             print("********** TRAIN GCN NETWORK **********")
-            if args.dataset in 'cora, reddit-self-loop, citeseer, pubmed':
+            if args.dataset in 'cora, citeseer, pubmed':
                 train_citation(gcn, g, features, labels, train_mask, test_mask, args)
             elif args.dataset == 'ppi':
                 train_ppi(gcn, train_dataloader, valid_dataloader, args)
@@ -111,14 +129,22 @@ def main(args):
 
             print("********** GCN MODEL: GCN_{}layer **********".format(gcn_model_layer))
             print("********** EXPERIMENT ITERATION: {} **********".format(repeat_time+1))
-            identifiable_nodes_1layer = set() # store nodes that can be identified after 1st layer
+            if args.dataset in 'cora, citeseer, pubmed':
+                identifiable_nodes_1layer = set() # store nodes that can be identified after 1st layer
+            elif args.dataset == 'ppi':
+                identifiable_nodes_1layer = np.array([])
+                embedding_id_rates_layer = np.zeros(len(train_dataset))
+                node_repeatability_rates_layer = np.zeros(len(train_dataset))
+                max_contributed_neighbour_recovered_rates_layer = np.zeros(len(train_dataset))
             for intermediate_layer in np.arange(1, gcn_model_layer+1):
-                identifiable_nodes_current_layer = set()  # store nodes that can be identified after current layer
+                if args.dataset in 'cora, citeseer, pubmed':
+                    identifiable_nodes_current_layer = set()  # store nodes that can be identified after current layer
                 print("********** INTERMEDIATE LAYER: {} **********".format(intermediate_layer))
                 if args.info == 'self-identity':
                     if args.dataset in 'cora, citeseer, pubmed':
                         print("********** COMPUTE NEIGHBOURHOOD NODES ATTRIBUTING AT MOST **********")
-                        max_contributed_nodes = compute_nodes_attribution(gcn, g, features, intermediate_layer, args)
+                        # max_contributed_nodes = compute_nodes_attribution_citation(gcn, g, features, intermediate_layer, args)
+                        max_contributed_nodes = np.zeros(g.number_of_nodes())
                         # prepare to build the regression model
                         embedding = gcn(g, features)[-intermediate_layer].clone().detach().to(device)
                         input_features = features.clone().detach().to(device)
@@ -217,24 +243,49 @@ def main(args):
                     elif args.dataset == 'ppi':
                         if args.knn == 1:
                             print("********** NEAREST NEIGHBOUR TO FIND CORRESPONDING INPUT **********")
-                            nn_list = np.zeros(0)   # indices of the found nearest neighbourhood of nodes
-                            for data in train_dataset:
-                                reg_output = reg(embedding)
-                            for node_ind in range(g.number_of_nodes()):
-                                neighbour_ind = (g.adjacency_matrix(transpose=True)[node_ind].to_dense()==1)
-                                neighbour_feat = input_features[neighbour_ind]
-                                node_reg_output = reg_output[node_ind]
-                                # Find Nearest Neighbour
-                                if args.regression_metric == 'l2':
-                                    dist = torch.norm(neighbour_feat - node_reg_output, dim=1, p=None)
-                                    nn = dist.topk(1, largest=False)
-                                elif args.regression_metric == 'cos':
-                                    node_reg_output = node_reg_output.expand_as(neighbour_feat)
-                                    dist = torch.nn.functional.cosine_similarity(neighbour_feat, node_reg_output)
-                                    nn = dist.topk(1, largest=False)
-                                # record the index of nn
-                                nn_list[node_ind] = g.adjacency_matrix(transpose=True)[node_ind]._indices()[0,nn.indices].item()
-                                print('Node_id: {} | Corresponding NN Node_id: {}'.format(node_ind, nn_list[node_ind]))
+                            for graph_id, data in enumerate(train_dataset):
+                                num_identifiable = 0.0
+                                num_repeatable = 0.0 # number of repeatable nodes of current graph
+                                num_nn_is_max_contributed_neighbour = 0.0
+                                identifiable_nodes_current_layer_set = set()
+                                identifiable_nodes_1layer_set = set()
+                                nn_list = np.zeros(data[0].number_of_nodes()) # indices of the found nearest neighbourhood of nodes
+
+                                reg_output = reg(data[0].ndata['embedding'].float())
+                                for node_ind in range(data[0].number_of_nodes()):
+                                    neighbour_ind = (data[0].adjacency_matrix(transpose=True)[node_ind].to_dense()==1)
+                                    neighbour_feat = data[0].nadata['embedding'][neighbour_ind]
+                                    node_reg_output = reg_output[node_ind]
+                                    # Find Nearest Neighbour
+                                    if args.regression_metric == 'l2':
+                                        dist = torch.norm(neighbour_feat - node_reg_output, dim=1, p=None)
+                                        nn = dist.topk(1, largest=False)
+                                    elif args.regression_metric == 'cos':
+                                        node_reg_output = node_reg_output.expand_as(neighbour_feat)
+                                        dist = torch.nn.functional.cosine_similarity(neighbour_feat, node_reg_output)
+                                        nn = dist.topk(1, largest=False)
+                                    # record the index of nn
+                                    nn_list[node_ind] = g.adjacency_matrix(transpose=True)[node_ind]._indices()[0,nn.indices].item()
+                                    print('Node_id: {} | Corresponding NN Node_id: {}'.format(node_ind, nn_list[node_ind]))
+                                for node_ind in range(data[0].number_of_nodes()):
+                                    if node_ind == nn_list[node_ind]:  # whether identifiable
+                                        num_identifiable += 1
+                                        identifiable_nodes_current_layer_set.add(node_ind)
+                                        if intermediate_layer==1:
+                                            identifiable_nodes_1layer_set.add(node_ind)
+                                if intermediate_layer==1:
+                                    identifiable_nodes_1layer = np.append(identifiable_nodes_1layer, identifiable_nodes_1layer_set)
+                                for node_ind in identifiable_nodes_current_layer_set:
+                                    if node_ind in identifiable_nodes_1layer[graph_id]:
+                                        num_repeatable+=1
+                                node_repeatability_rates_layer[graph_id] = num_repeatable / data[0].number_of_nodes()
+                                embedding_id_rates_layer[graph_id] = num_identifiable / data[0].number_of_nodes()
+                                print('Graph id {} | identifiability_rate = {} %'.format(graph_id, embedding_id_rates_layer[graph_id] * 100))
+                                print('Graph id {} | node_repeatability_rate = {} %'.format(graph_id, node_repeatability_rates_layer[graph_id] * 100))
+                            embedding_id_rates[intermediate_layer - 1, repeat_time] = np.mean(embedding_id_rates_layer)
+                            node_repeatability_rates[intermediate_layer - 1, repeat_time] = np.mean(embedding_id_rates_layer)
+                            max_contributed_neighbour_recovered_rates[intermediate_layer - 1, repeat_time] = 0
+
 
                 print("********** GCN MODEL: GCN_{}layer **********".format(gcn_model_layer))
                 print("********** EXPERIMENT ITERATION: {} **********".format(repeat_time+1))
@@ -246,13 +297,22 @@ def main(args):
                         num_repeatable = 0.0
                         num_nn_is_max_contributed_neighbour = 0.0
                         for node_ind in range(g.number_of_nodes()):
-                            if node_ind in nn_list[node_ind]: # whether identifiable
-                                num_identifiable+=1
-                                identifiable_nodes_current_layer.add(node_ind)
-                                if intermediate_layer==1:
-                                    identifiable_nodes_1layer.add(node_ind)
-                            elif max_contributed_nodes[node_ind] in nn_list[node_ind]:
-                                num_nn_is_max_contributed_neighbour+=1
+                            if args.knn == 1:
+                                if node_ind == nn_list[node_ind]:  # whether identifiable
+                                    num_identifiable += 1
+                                    identifiable_nodes_current_layer.add(node_ind)
+                                    if intermediate_layer == 1:
+                                        identifiable_nodes_1layer.add(node_ind)
+                                elif max_contributed_nodes[node_ind] == nn_list[node_ind]:
+                                    num_nn_is_max_contributed_neighbour += 1
+                            else:
+                                if node_ind in nn_list[node_ind]: # whether identifiable
+                                    num_identifiable+=1
+                                    identifiable_nodes_current_layer.add(node_ind)
+                                    if intermediate_layer==1:
+                                        identifiable_nodes_1layer.add(node_ind)
+                                elif max_contributed_nodes[node_ind] in nn_list[node_ind]:
+                                    num_nn_is_max_contributed_neighbour+=1
                         identifiability_rate = num_identifiable / g.number_of_nodes()
                         max_contributed_neighbour_recovered_rate = num_nn_is_max_contributed_neighbour / g.number_of_nodes()
                         for node_ind in identifiable_nodes_current_layer:
@@ -261,23 +321,24 @@ def main(args):
                         node_repeatability_rate = num_repeatable / len(identifiable_nodes_current_layer)
                     print('identifiability_rate = {} %'.format(identifiability_rate*100))
                     print('node_repeatability_rate = {} %'.format(node_repeatability_rate * 100))
+                    print('max_contributed_neighbour_recovered_rate = {} %'.format(max_contributed_neighbour_recovered_rate * 100))
                     embedding_id_rates[intermediate_layer-1, repeat_time] = identifiability_rate
-                    node_repeatablity_rates[intermediate_layer-1, repeat_time] = node_repeatability_rate
+                    node_repeatability_rates[intermediate_layer-1, repeat_time] = node_repeatability_rate
                     max_contributed_neighbour_recovered_rates[intermediate_layer-1, repeat_time] = max_contributed_neighbour_recovered_rate
 
         df_column_ind = models[gcn_model_layer-2]
         embedding_id_rates_df[df_column_ind] = np.mean(embedding_id_rates, axis=1)
-        node_repeatability_rates_df[df_column_ind] = np.mean(node_repeatablity_rates, axis=1)
+        node_repeatability_rates_df[df_column_ind] = np.mean(node_repeatability_rates, axis=1)
         max_contributed_neighbour_recovered_rates_df[df_column_ind] = np.mean(max_contributed_neighbour_recovered_rates, axis=1)
 
     if args.info == 'self-identity':
-        save_subpath = 'embedding_id_rates_' + args.dataset + '_'+ args.knn + '_-nn' + '.csv'
+        save_subpath = 'embedding_id_rates_' + args.dataset + '_'+ str(args.knn) + '_-nn' + '.csv'
         save_path = os.path.join(outputs_subdir, save_subpath)
         embedding_id_rates_df.to_csv(save_path)
-        save_subpath = 'repeatability_rates_' + args.dataset + '_' + args.knn +'_-nn' + '.csv'
+        save_subpath = 'repeatability_rates_' + args.dataset + '_' + str(args.knn) +'_-nn' + '.csv'
         save_path = os.path.join(outputs_subdir, save_subpath)
         node_repeatability_rates_df.to_csv(save_path)
-        save_subpath = 'max_contributed_neighbour_identifiable_rates_' + args.dataset + '_' + args.knn +'_-nn' + '.csv'
+        save_subpath = 'max_contributed_neighbour_identifiable_rates_' + args.dataset + '_' + str(args.knn) +'_-nn' + '.csv'
         save_path = os.path.join(outputs_subdir, save_subpath)
         max_contributed_neighbour_recovered_rates_df.to_csv(save_path)
 
@@ -291,7 +352,8 @@ if __name__ == '__main__':
     parser.add_argument('--regression_model', default='mlp', help='choose model structure from: slp, mlp or none')
     parser.add_argument('--regression_metric', default='cos', help='choose metric for regression and nearest neighbour from: l2 or cos')
     parser.add_argument('--knn', type=int, default=1, help='method to find the corresponding node among neighboring nodes after recovery, k=1,2 or 3')
-    parser.add_argument('--repeat_times', type=int, default=5, help='experiment repeating times for single layer')
+    parser.add_argument('--repeat_times', type=int, default=3, help='experiment repeating times for single layer')
+    parser.add_argument('--max_gcn_layer', type=int, default=6, help='the maxmal gcn models\'s layer')
     args = parser.parse_args()
 
     print(args)
