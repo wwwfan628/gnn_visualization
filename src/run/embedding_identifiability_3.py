09a5f85e6908fa795845ead3d550fbd4553202a6
wwwfan628 <wwwfan628@163.com>
2020-06-28 14:43:02 +0200

    debug embedding_identifiability_3.py: GPU

--- src/run/embedding_identifiability_3.py
+++ src/run/embedding_identifiability_3.py
@@ -331,11 +331,11 @@ def main(args):
                                 # Find k Nearest Neighbourhood
                                 if args.regression_metric == 'l2':
                                     dist = torch.norm(neighbour_feat.to(device) - node_regression_output.to(device), dim=1, p=None)
-                                    nn = dist.topk(args.knn, largest=False).to(device)
+                                    nn = dist.topk(args.knn, largest=False)
                                 elif args.regression_metric == 'cos':
                                     node_regression_output = node_regression_output.expand_as(neighbour_feat)
                                     dist = torch.nn.functional.cosine_similarity(neighbour_feat.to(device), node_regression_output.to(device))
-                                    nn = dist.topk(args.knn, largest=False).to(device)
+                                    nn = dist.topk(args.knn, largest=False)
                                 # record the index of nn
                                 nn_list[node_ind] = graph.adjacency_matrix(transpose=True)[node_ind]._indices()[0, nn.indices]
                                 print('Node_id: {} | Corresponding NN Node_id: {}'.format(node_ind, nn_list[node_ind]))
